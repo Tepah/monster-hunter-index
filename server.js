e5ca47851a6bc577bcca767f76e2a52cfe0cb345
Potipitak Pete <TepahT@gmail.com>
2022-04-23 23:21:39 -0700

    Getting started with database stuff

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send("Hello world!"));
+const port = process.env.PORT || 8082;
 
-// console.log that your server is up and running.
-app.listen(port, () => console.log('listening on port ${port}'));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-    res.send({ express:})
-})
+app.listen(port, () => console.log(`Server running on port ${port}`));
