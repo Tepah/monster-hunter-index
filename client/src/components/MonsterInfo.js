0cc583e16cc050042605dfc47a8cc78016b7342b
Potipitak Pete <TepahT@gmail.com>
2022-04-26 17:05:26 -0700

    Website not showing all info, gonna link server stuff then continue testing.

--- client/src/components/MonsterInfo.js
+++ client/src/components/MonsterInfo.js
@@ -1,19 +1,32 @@
 import React from "react";
 import logo from "../logo.svg";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export class MonsterInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      monster: "Dracula",
-      type: "Fiction",
-      games: ["Pokemon", "Naruto", "Demon Salver"],
-      summary: "This monster sucks blood for a living, is allergic to garlic, and can't stand werewolves taking their girls.",
-      weakness: ["3", "2", "1", "2", "0", "1", "3", "2", "0", "2", "O"]
+      monster: {}
     };
   }
 
+  componentDidMount() {
+    axios
+      .get('http://localhost:8082/api/monsters/'+this.props.match.params.name)
+      .then(res => {
+        this.setState({
+          monster: res.data
+        })
+      })
+      .catch(err => {
+        console.log("Error from MonsterInfo");
+      })
+  };
+
   render() {
+    const monster = this.state.monster;
+
     return (
       <div class="card">
         <div className="Monster-info card-body">
@@ -21,55 +34,55 @@ export class MonsterInfo extends React.Component {
             alt="The Monster's type" />
           <img className="Monster-image" src={logo}
             alt="The Monster in Question" />
-          <h2 className="Monster-name card-title">{this.state.monster}</h2>
-          <div className="monster-type">Monster Type: {this.state.type}</div>
+          <h2 className="Monster-name card-title">{monster.name}</h2>
+          <div className="monster-type">Monster Type: {monster.type}</div>
           <div className="games-included">
             Featured in:
             <ul className="Gamelist">
               <li>Pokemon</li>
             </ul>
           </div>
-          <div className="Summary">{this.state.summary}</div>
+          <div className="Summary">{monster.summary}</div>
           <div>
             <h3 className="Weakness">
               <div className="container">
                 <div className="row">
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[0]} type={require("../elements/fire.png")} />
+                    <Weakness value={monster.weakness[0]} type={require("../elements/fire.png")} />
                   </div>
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[1]} type={require("../elements/water.png")} />
+                    <Weakness value={monster.weakness[1]} type={require("../elements/water.png")} />
                   </div>
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[2]} type={require("../elements/thunder.png")} />
+                    <Weakness value={monster.weakness[2]} type={require("../elements/thunder.png")} />
                   </div>
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[3]} type={require("../elements/ice.png")} />
+                    <Weakness value={monster.weakness[3]} type={require("../elements/ice.png")} />
                   </div>
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[4]} type={require("../elements/dragon.png")} />
+                    <Weakness value={monster.weakness[4]} type={require("../elements/dragon.png")} />
                   </div>
                 </div>
                 <div className="row">
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[5]} type={require("../elements/poison.png")} />
+                    <Weakness value={monster.weakness[5]} type={require("../elements/poison.png")} />
                   </div>
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[6]} type={require("../elements/sleep.png")} />
+                    <Weakness value={monster.weakness[6]} type={require("../elements/sleep.png")} />
                   </div>
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[7]} type={require("../elements/paralysis.png")} />
+                    <Weakness value={monster.weakness[7]} type={require("../elements/paralysis.png")} />
                   </div>
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[8]} type={require("../elements/explosive.png")} />
+                    <Weakness value={monster.weakness[8]} type={require("../elements/explosive.png")} />
                   </div>
                   <div className="col-lg-2">
-                    <Weakness value={this.state.weakness[9]} type={require("../elements/stun.png")} />
+                    <Weakness value={monster.weakness[9]} type={require("../elements/stun.png")} />
                   </div>
                 </div>
                 <div className="row">
                   <div className="col-lg-10">
-                    <Weakness value={this.state.weakness[10]} type={require("../elements/sharp.png")} />
+                    <Weakness value={monster.weakness[10]} type={require("../elements/sharp.png")} />
                   </div>
                 </div>
               </div>
@@ -80,6 +93,7 @@ export class MonsterInfo extends React.Component {
     );
   }
 }
+
 class Weakness extends React.Component {
   render() {
     return (
@@ -90,6 +104,7 @@ class Weakness extends React.Component {
     );
   }
 }
+
 function Element(props) {
   return <img className="element" src={props.type} alt="elem" />;
 }
