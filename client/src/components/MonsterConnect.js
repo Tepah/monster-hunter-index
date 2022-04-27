33fae479593a983f7230f03675848f9b8bb2fe44
Potipitak Pete <TepahT@gmail.com>
2022-04-27 12:44:38 -0700

    Starting to see where this is failing

--- client/src/components/MonsterConnect.js
+++ client/src/components/MonsterConnect.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../logo.svg";
 import axios from 'axios';
 import { MonsterInfo } from "./MonsterInfo";
@@ -8,28 +8,58 @@ export class MonsterConnect extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            monsters: {
-                "rathian": {
-                    "name": "Rathian",
-                    "summary": "a drag",
-                    "type": "wyvern",
-                    "weakness": ["1", "2", "3", "1", "2", "3", "1", "2", "3", "1"]
-                },
-            }
-        };
-    }
+            monsters: {},
+        }
+    };
+    
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:8082/api/monsters')
+            .then(res => {
+                this.setState({
+                    monsters: res.data
+                })
+            })
+            .catch(err =>{
+                console.log("Error from MonsterConnect");
+            })
+    };
 
 
     render() {
-        let monsters = this.state.monsters;
+        const list = ["1", "2"]
+        const monsters = {
+            "rathian": {
+                "name": "Rathian",
+                "type": "Wyvern",
+                "summary": "\"A big dragon\"",
+                "games": [
+                    "Monster Hunter World"
+                ],
+                "weakness": [
+                    "0",
+                    "2",
+                    "3",
+                    "1",
+                    "2",
+                    "3",
+                    "0",
+                    "2",
+                    "1",
+                    "0",
+                    "O"
+                ]
+            }
+        }
         return (
             <div class="container">
           <div class="row">
             <div class="col-sm-3">
-              <Monsterlist monsters={Object.keys(monsters)} />
+              <Monsterlist monsters={list} />
             </div>
             <div class="col-9">
-              <MonsterInfo monsters={ monsters } monster={ monsters.rathian } />
+              <MonsterInfo monsters={ monsters } monster={ monsters["rathian"] } />
             </div>
           </div>
         </div>
